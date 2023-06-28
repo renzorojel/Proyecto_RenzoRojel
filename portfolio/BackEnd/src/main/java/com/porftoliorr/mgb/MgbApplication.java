bf5bac4d0190837918714ff17b1c3aa6780e6277
renzorojel <123786371+renzorojel@users.noreply.github.com>
2023-06-28 15:20:55 -0300

    backend neww

--- portfolio/BackEnd/src/main/java/com/porftoliorr/mgb/MgbApplication.java
+++ portfolio/BackEnd/src/main/java/com/porftoliorr/mgb/MgbApplication.java
@@ -1,4 +1,4 @@
-package com.portfoliorr.mgb;
+package com.porftoliorr.mgb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- portfolio/BackEnd/src/test/java/com/porftoliorr/mgb/MgbApplicationTests.java
+++ portfolio/BackEnd/src/test/java/com/porftoliorr/mgb/MgbApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfoliorr.mgb;
+package com.porftoliorr.mgb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
